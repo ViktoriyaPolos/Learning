f308cffdde19ebdb96d22c36cb5f079e5a5528f7
ViktoriyaPolos <viktoriyapolos97@gmail.com>
2023-03-28 19:50:41 +0100

    todays classes

--- JS_Fundamentals/almost_everything_is_a_function.js
+++ JS_Fundamentals/almost_everything_is_a_function.js
@@ -4,3 +4,19 @@ console.log(typeof Object);
 
 class Product {};
 console.log(typeof Product);
+
+
+ // Function without an output
+ function printSum(a, b) {
+    console.log(a + b)
+ };
+ printSum(2, 3);
+ printSum(2); // the second value will be undefined so the result is Not a Number
+ printSum(2, 10, 4, 5, 6, 7);
+
+ // Function with an output
+ function sum(a, b = 0) {
+    return a + b
+ };
+ console.log(sum(2, 3));
+ console.log(sum(2));
