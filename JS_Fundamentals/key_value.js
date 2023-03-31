19e945864ae618171ceca9b20531218e99d26c72
ViktoriyaPolos <viktoriyapolos97@gmail.com>
2023-03-31 20:43:26 +0100

    today's classes

--- JS_Fundamentals/key_value.js
+++ JS_Fundamentals/key_value.js
@@ -1,2 +1,30 @@
- const greeting = 'Hello'; // lexical context => in this context, it's at the level of the arquive we're working
- console.log(greeting);
+// Keys & Values Pairings
+
+// Example 1:
+const greeting = 'Hello'; // lexical context (physical place where the variable is defined in the code)=> in this context, it's at the level of the arquive we're working
+console.log(greeting);
+
+// here the key is greeting and the value is Hello. If we try to define it again: 
+
+const greeting = 'Hi';
+ 
+// Example 2:
+function exec() {
+    const greeting = 'Hey there' // this is another lexical context, within a function
+    return greeting 
+};
+console.log(exec());
+
+// even tho we defined the same key as in ex.1, because here it's in a function, the scope/lexical context is diferent
+
+// Example 3:
+const Client = {
+    name: 'Paul',
+    age: 32, 
+    weight: 90,
+    address: {
+        Street: 'Wathever nº 5',
+        City: 'Very Cool City'
+    }
+};
+console.log(Client);
