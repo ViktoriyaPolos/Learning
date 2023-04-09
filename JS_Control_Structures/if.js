b7206a0a2c1290be54cd4d2938dfb39e56c7646d
ViktoriyaPolos <viktoriyapolos97@gmail.com>
2023-04-09 22:27:50 +0100

    classes

--- JS_Control_Structures/if.js
+++ JS_Control_Structures/if.js
@@ -1,1 +1,28 @@
-// ... super related to the fundamentals
+function onlyGoodNews(grade) {
+    if(grade >= 7) {                     // in this example, and in many other cases, JS operates based on true/false
+        console.log('Pass with ' + grade)
+    };
+};
+onlyGoodNews(8.1);
+onlyGoodNews(6.1); // this one wasn0t printed as it doesn't satisfy the conditional
+
+function ifTrueISpeak(value) {
+    if(value) {                         // here as well, it will convert the value to true/false to perform the operation
+        console.log('Its true... ' + value)
+    };
+};
+// here only the ones considered trues will be printed onto the console
+ifTrueISpeak();
+ifTrueISpeak(null);
+ifTrueISpeak(undefined);
+ifTrueISpeak(NaN);
+ifTrueISpeak('');
+ifTrueISpeak(0);
+ifTrueISpeak(-1);
+ifTrueISpeak(' ');
+ifTrueISpeak('?');
+ifTrueISpeak([]);
+ifTrueISpeak([1,2]);
+ifTrueISpeak({});
+
+// this also shows that ut doesn't need to be of boolean type, JS will "convert" to boolean 
